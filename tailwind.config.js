91f75c4154c6cbf6959d67ddf135fdca740b0fa9
tbfarhannh <tubagusfn14@gmail.com>
2025-02-10 01:24:17 +0700

    update root

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}", // pastikan ini sesuai dengan struktur direktori kamu
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
